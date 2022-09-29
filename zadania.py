97662b4e99701bad0afcc75a43f22ecd7600d5d1
Nalsiq <burdakm07@gmail.com>
2022-09-29 12:03:18 +0200

    .

---
+++ zadania.py
@@ -0,0 +1,34 @@
+print("Hello world") #gdy chcesz wyswietlic wyraz piszesz ("cos")
+
+zmienna=5
+print(zmienna) #gdy chcesz napisac zeby program wykonal cos piszesz (cos)
+
+rzeczywista=10.5 # liczby po przecinku tutaj to po kropce 10,5 nie 10.5 tak
+
+napis="hello world"
+napis2='hell world'
+napis3="hello 'world' hello"
+
+print(napis)
+print(napis2)
+print(napis3)
+
+
+print(" wartosc zmiennej to:", zmienna)
+
+print(" wartosc zmiennej to:")
+print(zmienna)
+
+print("wartosc zmiennej to: %.2f" %zmienna)
+
+print(f"wartsoc zmiennej wynosi :{zmienna} ")
+print("Wartosc zmiennej to: "+ str(zmienna))
+
+
+print(input("ile masz lat?"))
+
+wiek=input("ile masz lat?")
+
+wiek=int(input("ile masz lat?"))
+
+    
