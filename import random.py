b69a71803e681d38dc936003041887ae41b54938
Nalsiq <burdakm07@gmail.com>
2022-10-20 12:21:19 +0200

    .

---
+++ import random.py
@@ -0,0 +1,76 @@
+from operator import truediv
+import random
+
+#for licznik in range(3):
+ #   los=random.randint(1,50)
+  #  print(f"Wylosowana liczba nr {licznik+1} to {los}")
+#
+ #   zmienna=10
+
+#for licznik in range(zmienna):
+ #   los=random.randint(1,50)
+  #  print(f"Wylosowana liczba nr {licznik} to {los}")
+
+
+#print("___________")
+#for licznik in range(1,5):
+ #   los=random.randint(1,50)
+  #  print(f"Wylosowana liczba nr {licznik} to {los}")
+
+
+
+
+   # print("___________")
+#for licznik in range(1,10, 2):
+ #   los=random.randint(1,50)
+  #  print(f"Wylosowana liczba nr {licznik} to {los}")
+
+
+
+   # print("___________")
+#for licznik in range(100, 1, -1):
+ #   los=random.randint(1,50)
+  #  print(f"Wylosowana liczba nr {licznik} to {los}")
+
+
+
+
+
+for liczba in range(1, 101):
+    if liczba%2==0:
+        print(f"Liczba {liczba} jest podzielna")
+
+
+
+for litera in "kapelusz":
+    if litera=="l":
+        break
+    print(litera)
+
+for litera in "kapelusz":
+        if litera=="l":
+            continue
+        print(litera)
+
+
+
+liczba=0
+
+while liczba<10:
+    print(f"licza to : {liczba}")
+    liczba+=1
+
+
+
+
+
+
+while liczba1<=0:
+    liczba1=int(input("Podaj liczbe jeszce raz: "))
+
+
+
+
+
+#while True:
+ #   print("to prawda")
