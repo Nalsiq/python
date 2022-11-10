1ca33adea94cae62b201a5823fc3a243a5df2527
Nalsiq <burdakm07@gmail.com>
2022-11-10 12:20:48 +0100

    ..

---
+++ lottt.py
@@ -0,0 +1,21 @@
+import random
+random.randint
+# losowanie z ilu tam liczb, i porowaninie jes z liczbami od uzytkownika, robie uzytkownik ma kase, i ma do wyboru trzy kupony, o roznej wartosci, losowanie numerow, lista uzywaj
+# jesli masz te same numery wygrales
+slownik={
+    'liczba1': input("Podaj liczbe1"),
+    'liczba2': input("Podaj liczbe2"),
+    'liczba3': input("Podaj liczbe3")
+}
+uzytkownik=input(slownik)
+komputer=random.randint(0,50)
+
+def sprawdzanie(uzytkownik, komputer):
+    return
+
+print(uzytkownik)
+
+
+
+
+
---
+++ typy.py
@@ -0,0 +1,33 @@
+import random
+print("lista")
+lista=[1,2,3,4,5]
+print(lista)
+lista.append(6)
+print(lista)
+lista.remove(6)
+print(lista)
+print(lista[2])
+
+
+
+print("tuple")
+tupla=(1,2,3,4)
+print(tupla.index(2))
+
+
+
+print("slowik")
+slownik={
+    'jeden':1,
+    'dwa': 2,
+    'trzy': 3
+}
+print(slownik['jeden'])
+
+print("zbiory")
+zbior={1,2,2,3}
+print(zbior)
+zbior.add(5)
+print(zbior)
+zbior.remove(2)
+print(zbior)
